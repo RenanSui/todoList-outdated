159ffcfb4389f761799994b2dcc939a79722d35f
RenanSui <101724157+RenanSui@users.noreply.github.com>
2022-09-16 12:55:09 -0300

    Updating handle checks

--- App.js
+++ App.js
@@ -5,8 +5,9 @@ const clearItems = document.querySelector('.clear');
 
 // event listeners
 window.addEventListener('DOMContentLoaded', () => renderList());
+window.addEventListener('click', (e) => handleCircle(e));
 myForm.addEventListener('submit', (e) => handleSubmit(e));
-listContainer.addEventListener('click', (e) =>UPDATE_OR_REMOVE_fromLocalStorage(e));
+listContainer.addEventListener('click', (e) => UPDATE_OR_REMOVE_fromLocalStorage(e));
 clearItems.addEventListener('click', () => clearAllItems());
 
 // render list
@@ -18,30 +19,123 @@ const renderList = () => {
 	for (let i = 0; i < items.length; i++) {
 		list += `
 		<!-- Start of single item-->
-	    <div class="container-item" id="${items[i].id}">
-		<div class="circle list-options">
-			<i class="fa-regular fa-circle-check"></i>
-			<i class="fa-solid fa-pen-to-square"></i>
-			<i class="fa-solid fa-trash"></i>
+	    <div class="container-item">
+		<i class="fa-regular fa-circle-check checkItem"></i>
+		<div class="circle">
+			<div class="list-options" id="${items[i].id}">
+				<i class="fa-solid fa-square-check"></i>
+				<i class="fa-solid fa-pen-to-square"></i>
+				<i class="fa-solid fa-trash"></i>
+			</div>
 		</div>
 	    <p>${items[i].value}</p>
 	    </div>
 		<!-- End of single item-->`;
 	}
 
-	// Change circle check color
-	window.addEventListener('click', (e) => {
-		if (e.target.classList.contains('fa-circle-check')) {
-			document.querySelector('.fa-circle-check').classList.toggle('red');
-			console.log('circle check alterado');
-		}
-	});
-
 	// render list on the HTML
 	listContainer.innerHTML = list;
 };
 
-// functions
+// LOCAL STORAGE
+
+const UPDATE_OR_REMOVE_fromLocalStorage = (e) => {
+	// Get element id and class
+	const id = e.target.parentElement.id;
+	const target = e.target.classList;
+
+	// Get the local Storage Items
+	let items = getLocalStorage();
+
+	// *****TARGET EVENT LISTENERS*****
+	// Remove item clicked
+	if (target.contains('fa-trash')) {
+		items = items.filter((item) => {
+			if (item.id != id) {
+				return item;
+			}
+		});
+
+		// Set new local Store
+		newLocalStore(items)
+		console.log('item deletado!');
+	}
+
+	// Update item clicked
+	if (target.contains('fa-pen-to-square')) {
+		console.log('item alterado!');
+	}
+
+};
+
+const addToLocalStorage = (id, value) => {
+	const listItems = { id, value };
+	let items = getLocalStorage();
+
+	items.push(listItems);
+	localStorage.setItem('list', JSON.stringify(items));
+	renderList();
+};
+
+const removeFromLocalStorage = (id, value) => {
+
+}
+
+const updateFromLocalStorage = (id, value) => {
+	
+}
+
+const getLocalStorage = () => {
+	// return the current list
+	return localStorage.getItem('list')
+		? JSON.parse(localStorage.getItem('list'))
+		: [];
+};
+
+const handleCircle = (e) => {
+	const target = e.target.classList;
+
+	// Show circle options
+	if (target.contains('circle')) {
+		const id = e.target.childNodes[1].id;
+		const listOptions = document.getElementById(`${id}`).classList;
+		listOptions.add('list-options-display-block');
+	}
+
+	// Hide circle options
+	if (target.contains('fa-square-check')) {
+		const id = e.target.parentElement.id;
+		const listOptions = document.getElementById(`${id}`).classList;
+		listOptions.remove('list-options-display-block');
+	}
+
+	// Show circle check
+	if (target.contains('fa-square-check')) {
+		const id = e.target.parentElement.id;
+		const circle = document.getElementById(`${id}`).parentElement.previousElementSibling;
+		circle.classList.add('checkItemDisplay')
+	}
+
+	// Hide circle check
+	if (target.contains('fa-circle-check')) {
+		const id = e.target.nextElementSibling.childNodes[1].id;
+		const checkItem = document.getElementById(`${id}`).parentElement.previousElementSibling;
+		console.log(checkItem);
+		checkItem.classList.remove('checkItemDisplay');
+	}
+}
+
+const clearAllItems = () => {
+	let items = [];
+	localStorage.setItem('list', JSON.stringify(items));
+	renderList();
+};
+
+const newLocalStore = (items) => {
+	localStorage.setItem('list', JSON.stringify(items));
+	renderList();
+}
+
 const handleSubmit = (e) => {
 	e.preventDefault();
 
@@ -58,55 +152,4 @@ const handleSubmit = (e) => {
 	// Clear input field
 	const input = document.querySelector('#list');
 	input.value = '';
-};
-
-// LOCAL STORAGE
-const addToLocalStorage = (id, value) => {
-	const listItems = { id, value };
-	let items = getLocalStorage();
-
-	items.push(listItems);
-	localStorage.setItem('list', JSON.stringify(items));
-	renderList();
-};
-
-const UPDATE_OR_REMOVE_fromLocalStorage = (e) => {
-	// Get element id and class
-	const id = e.target.parentElement.id;
-	const target = e.target.classList;
-
-	// Get the local Storage Items
-	let items = getLocalStorage();
-
-	// ***TARGET EVENT LISTENERS***
-	// Remove item clicked
-	if (target.contains('fa-trash')) {
-		items = items.filter((item) => {
-			if (item.id != id) {
-				return item;
-			}
-		});
-		console.log('item deletado!');
-	}
-	// Update item clicked
-	if (target.contains('fa-pen-to-square')) {
-		console.log('item alterado!');
-	}
-
-	// Set new local store
-	localStorage.setItem('list', JSON.stringify(items));
-	renderList();
-};
-
-const getLocalStorage = () => {
-	// return the current list
-	return localStorage.getItem('list')
-		? JSON.parse(localStorage.getItem('list'))
-		: [];
-};
-
-const clearAllItems = () => {
-	let items = [];
-	localStorage.setItem('list', JSON.stringify(items));
-	renderList();
 };
