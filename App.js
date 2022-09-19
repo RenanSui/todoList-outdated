8ecac178d6522750e7740f1c8225b684b6ca06ce
RenanSui <101724157+RenanSui@users.noreply.github.com>
2022-09-19 15:30:28 -0300

    todo list v1.0 maybe?

--- App.js
+++ App.js
@@ -3,15 +3,19 @@ const myForm = document.querySelector('.myForm');
 const listContainer = document.querySelector('.container');
 const clearItems = document.querySelector('.clear');
 const itemsFooter = document.querySelector('.itemsFooter');
+const myFormEdit = document.querySelector('.myFormEdit');
+const alertBox = document.querySelector('.alertBox');
+
+// global variables
+let editID = '';
 
 // event listeners
 window.addEventListener('DOMContentLoaded', () => renderList());
 window.addEventListener('click', (e) => handleCircle(e));
 myForm.addEventListener('submit', (e) => handleSubmit(e));
-listContainer.addEventListener('click', (e) =>
-	UPDATE_OR_REMOVE_fromLocalStorage(e)
-);
+listContainer.addEventListener('click', (e) => handleRemove(e));
 clearItems.addEventListener('click', () => clearAllItems());
+myFormEdit.addEventListener('submit', (e) => handleEditSubmit(e));
 
 // render list
 const renderList = () => {
@@ -24,10 +28,10 @@ const renderList = () => {
 		list += `
 		<!-- Start of single item-->
 	    <div class="container-item">
-		<i class="fa-regular fa-circle-check checkItem"></i>
+		<!--<i class="fa-regular fa-circle-check checkItem"></i>-->
 		<div class="circle">
 			<div class="list-options" id="${items[i].id}">
-				<i class="fa-solid fa-square-check"></i>
+			<!--<i class="fa-solid fa-square-check"></i>-->
 				<i class="fa-solid fa-pen-to-square"></i>
 				<i class="fa-solid fa-trash"></i>
 			</div>
@@ -48,7 +52,7 @@ const renderList = () => {
 
 // LOCAL STORAGE
 
-const UPDATE_OR_REMOVE_fromLocalStorage = (e) => {
+const handleRemove = (e) => {
 	// Get element id and classes
 	const id = e.target.parentElement.id;
 	const target = e.target.classList;
@@ -64,7 +68,14 @@ const UPDATE_OR_REMOVE_fromLocalStorage = (e) => {
 
 	// Update item clicked
 	if (target.contains('fa-pen-to-square')) {
-		console.log('item alterado!');
+		// hide list options
+		const listOptions = document.getElementById(`${id}`).classList;
+		listOptions.remove('list-options-display-block');
+
+		// show edit form
+		myForm.classList.add('myFormDisplayNone');
+		myFormEdit.classList.add('myFormEditDisplayBlock');
+		editID = id;
 	}
 };
 
@@ -85,9 +96,26 @@ const removeFromLocalStorage = (id, list) => {
 	});
 	// Set new local Store
 	newLocalStore(newList);
+
+	// alert
+	const alertText = 'Item deleted!';
+	const alertClass = 'alertDanger';
+	handleAlert(alertText, alertClass);
 };
 
-const updateFromLocalStorage = (id, value) => {};
+const updateFromLocalStorage = (id, value) => {
+
+	let items = getLocalStorage();
+
+	items = items.map((item) => {
+		if (item.id == id) {
+			item.value = value;
+		}
+		return item;
+	});
+	localStorage.setItem('list', JSON.stringify(items));
+	renderList();
+};
 
 const getLocalStorage = () => {
 	// return the current list
@@ -114,12 +142,12 @@ const handleCircle = (e) => {
 	}
 
 	// Show circle check
-	if (target.contains('fa-square-check')) {
-		const id = e.target.parentElement.id;
-		const circle = document.getElementById(`${id}`).parentElement
-			.previousElementSibling;
-		circle.classList.add('checkItemDisplay');
-	}
+	// if (target.contains('fa-square-check')) {
+	// 	const id = e.target.parentElement.id;
+	// 	const circle = document.getElementById(`${id}`).parentElement
+	// 		.previousElementSibling;
+	// 	circle.classList.add('checkItemDisplay');
+	// }
 
 	// Hide circle check
 	if (target.contains('fa-circle-check')) {
@@ -149,13 +177,63 @@ const handleSubmit = (e) => {
 	const value = document.querySelector('#list').value;
 	const id = new Date().getTime();
 	if (value === '') {
-		alert('Defina um item!');
+		// alert
+		const alertText = 'Type something!';
+		const alertClass = 'alertWarning';
+		handleAlert(alertText, alertClass);
 	} else {
 		addToLocalStorage(id, value);
-		console.log('item foi alocado!');
+		// alert
+		const alertText = 'Item added!';
+		const alertClass = 'alertAdded';
+		handleAlert(alertText, alertClass);
 	}
 
 	// Clear input field
 	const input = document.querySelector('#list');
 	input.value = '';
+};
+
+const handleEditSubmit = (e) => {
+	e.preventDefault();
+
+	// Editing data
+	const value = document.querySelector('#listEdit').value;
+
+	if (value === '') {
+		// alert
+		const alertText = 'Type something!';
+		const alertClass = 'alertDanger';
+		handleAlert(alertText, alertClass);
+	} else {
+		updateFromLocalStorage(editID, value);
+
+		// alert
+		const alertText = 'Item edited!';
+		const alertClass = 'alertEdited';
+		handleAlert(alertText, alertClass);
+
+		// hide edit form
+		myForm.classList.remove('myFormDisplayNone');
+		myFormEdit.classList.remove('myFormEditDisplayBlock');
+	}
+
+	// Clear input field
+	const input = document.querySelector('#listEdit');
+	input.value = '';
+};
+
+const handleAlert = (alertText, alertClass) => {
+	// show alert box
+	alertBox.classList.add('alertBoxDisplay');
+	alertBox.classList.add(alertClass);
+	console.log(alertClass);
+	alertBox.innerText = alertText;
+
+	// hide alert box
+	setTimeout(() => {
+		alertBox.classList.remove('alertBoxDisplay');
+		alertBox.classList.remove(alertClass);
+		alertBox.innerText = '';
+	}, 3 * 1000);
 };
